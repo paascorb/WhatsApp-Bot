2f513e2dd6042fa4861835a8a1a9d3b2ff48640b
paascorb <56049659+paascorb@users.noreply.github.com>
2023-12-18 18:41:50 +0100

    Update webhook.py

--- app/webhook.py
+++ app/webhook.py
@@ -1,11 +1,86 @@
 import os
 import requests
 import flask
-from whatsapp_client import *
+import json
+
+class WhatsAppWrapper:
+
+    API_URL = "https://graph.facebook.com/v18.0/"
+    WHATSAPP_API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
+    WHATSAPP_CLOUD_NUMBER_ID = os.environ.get("WHATSAPP_CLOUD_NUMBER_ID")
+
+    def __init__(self):
+        self.headers = {
+            "Authorization": f"Bearer {self.WHATSAPP_API_TOKEN}",
+            "Content-Type": "application/json",
+        }
+        self.API_URL = self.API_URL + self.WHATSAPP_CLOUD_NUMBER_ID
+
+    def send_template_message(self, template_name, language_code, phone_number):
+
+        payload = {
+            "messaging_product": "whatsapp",
+            "to": phone_number,
+            "type": "template",
+            "template": {
+                "name": template_name,
+                "language": {
+                    "code": language_code
+                }
+            }
+        }
+        print(self.WHATSAPP_API_TOKEN)
+        response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
+        print(response)
+        print(response.status_code)
+        print(response.text)
+
+        assert response.status_code == 200, "Error sending message"
+        return response.status_code
+    
+    def send_text_message(self,message, phone_number):
+        payload = {
+            "messaging_product": 'whatsapp',
+            "to": phone_number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": message
+            }
+        }
+        response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
+        print(response.status_code)
+        print(response.text)
+        assert response.status_code == 200, "Error sending message"
+        return response.status_code
+    
+    def process_notification(self, data):
+        entries = data["entry"]
+        for entry in entries:
+            for change in entry["changes"]:
+                value = change["value"]
+                if value:
+                    if "messages" in value:
+                        for message in value["messages"]:
+                            if message["type"] == "text":
+                                from_no = message["from"]
+                                message_body = message["text"]["body"]
+                                prompt = message_body
+                                print(f"Ack from FastAPI-WtsApp Webhook: {message_body}")
+                                return {
+                                    "statusCode": 200,
+                                    "body": prompt,
+                                    "from_no": from_no,
+                                    "isBase64Encoded": False
+                                }
+
+        return {
+            "statusCode": 403,
+            "body": json.dumps("Unsupported method"),
+            "isBase64Encoded": False
+        }
 
 app = flask.Flask(__name__)
-
-nest_asyncio.apply()
 
 WHATSAPP_HOOK_TOKEN = os.environ.get("WHATSAPP_HOOK_TOKEN")
 
