f886fbb69bc202a6d46e8d940584c393c29823ae
paascorb <56049659+paascorb@users.noreply.github.com>
2023-12-18 18:07:02 +0100

    Add files via upload

---
+++ app/webhook.py
@@ -0,0 +1,48 @@
+import os
+import requests
+import json
+import config
+from fastapi import FastAPI, Request, Response
+from dotenv import load_dotenv
+import uvicorn
+import nest_asyncio
+
+nest_asyncio.apply()
+
+load_dotenv("creds.env")
+app = FastAPI()
+
+%run "Whatsapp API.ipynb"
+
+WHATSAPP_HOOK_TOKEN = os.environ.get("WHATSAPP_HOOK_TOKEN")
+
+@app.get("/")
+def I_am_alive():
+    return "¡Estoy vivo!"
+
+@app.get("/webhook/")
+def subscribe(request: Request):
+    print("subscribe ha sido llamado")
+    if request.query_params.get('hub.verify_token') == WHATSAPP_HOOK_TOKEN:
+        return int(request.query_params.get('hub.challenge'))
+    return "Authentication failed. Invalid Token."
+
+@app.post("/webhook/")
+async def callback(request: Request):
+    print("callback ha sido llamado")
+    wtsapp_client = WhatsAppClient()
+    data = await request.json()
+    print ("We received " + str(data))
+    response = wtsapp_client.process_notification(data)
+    if response["statusCode"] == 200:
+        if response["body"] and response["from_no"]:
+            reply = response["body"]
+            print ("\nreply is:"  + reply)
+            wtsapp_client.send_text_message(message=reply, phone_number=response["from_no"], )
+            print ("\nreply is sent to whatsapp cloud:" + str(response))
+
+    return {"status": "success"}, 200
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
