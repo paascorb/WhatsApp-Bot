9be24a529df1da7f1abd72ec418907fc2c2203c7
paascorb <56049659+paascorb@users.noreply.github.com>
2023-12-18 20:03:23 +0100

    Update webhook.py

--- app/webhook.py
+++ app/webhook.py
@@ -95,20 +95,21 @@ async def subscribe(request: Request):
         if request.query_params.get('hub.verify_token') == WHATSAPP_HOOK_TOKEN:
             return int(request.query_params.get('hub.challenge'))
         print("Hola2")
-        return "Authentication failed. Invalid Token."
-    wtsapp_client = WhatsAppClient()
-    data = await request.json()
-    print ("We received " + str(data))
-    response = wtsapp_client.process_notification(data)
-    if response["statusCode"] == 200:
-        if response["body"] and response["from_no"]:
-            openai_client = OpenAIClient()
-            reply = openai_client.complete(prompt=response["body"])
-            print ("\nreply is:"  + reply)
-            wtsapp_client.send_text_message(message=reply, phone_number=response["from_no"], )
-            print ("\nreply is sent to whatsapp cloud:" + str(response))
-
-    return jsonable_encoder({"status": "success"}, 200)
+        wtsapp_client = WhatsAppClient()
+        data = await request.json()
+        print ("We received " + str(data))
+        response = wtsapp_client.process_notification(data)
+        if response["statusCode"] == 200:
+            if response["body"] and response["from_no"]:
+                openai_client = OpenAIClient()
+                reply = openai_client.complete(prompt=response["body"])
+                print ("\nreply is:"  + reply)
+                wtsapp_client.send_text_message(message=reply, phone_number=response["from_no"], )
+                print ("\nreply is sent to whatsapp cloud:" + str(response))
+    
+        return jsonable_encoder({"status": "success"}, 200)
+        #return "Authentication failed. Invalid Token."
+    
 
 @app.post("/webhook/")
 async def process_notifications(request: Request):
