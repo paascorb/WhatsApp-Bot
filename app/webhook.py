06e093109355f97ac536cfd88cd1c2d96d92acf6
paascorb <56049659+paascorb@users.noreply.github.com>
2023-12-18 19:11:45 +0100

    Update webhook.py

--- app/webhook.py
+++ app/webhook.py
@@ -86,26 +86,25 @@ WHATSAPP_HOOK_TOKEN = os.environ.get("WHATSAPP_HOOK_TOKEN")
 @app.get("/")
 def I_am_alive():
     return "¡Estoy vivo!"
-
+    
 @app.get("/webhook/")
 def subscribe(request: Request):
-    print("subscribe ha sido llamado")
     if request.query_params.get('hub.verify_token') == WHATSAPP_HOOK_TOKEN:
         return int(request.query_params.get('hub.challenge'))
     return "Authentication failed. Invalid Token."
 
 @app.post("/webhook/")
-async def callback(request: Request):
-    print("callback ha sido llamado")
-    wtsapp_client = WhatsAppWrapper()
+async def process_notifications(request: Request):
+    wtsapp_client = WhatsAppClient()
     data = await request.json()
     print ("We received " + str(data))
     response = wtsapp_client.process_notification(data)
     if response["statusCode"] == 200:
         if response["body"] and response["from_no"]:
-            reply = response["body"]
+            openai_client = OpenAIClient()
+            reply = openai_client.complete(prompt=response["body"])
             print ("\nreply is:"  + reply)
             wtsapp_client.send_text_message(message=reply, phone_number=response["from_no"], )
             print ("\nreply is sent to whatsapp cloud:" + str(response))
 
-    return {"status": "success"}, 200
+    return jsonable_encoder({"status": "success"}, 200)
