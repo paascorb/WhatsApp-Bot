0bb9ac5c6abef62dd4387363a3393f7ecda277e0
paascorb <56049659+paascorb@users.noreply.github.com>
2023-12-19 22:01:36 +0100

    Add files via upload

--- app/webhook.py
+++ app/webhook.py
@@ -1,114 +1,62 @@
 import os
-from fastapi import FastAPI, Request, Response
-from fastapi.encoders import jsonable_encoder
 
-class WhatsAppWrapper:
+from flask import Flask, jsonify, request
+from app.whatsapp_client import WhatsAppWrapper
 
-    API_URL = "https://graph.facebook.com/v18.0/"
-    WHATSAPP_API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
-    WHATSAPP_CLOUD_NUMBER_ID = os.environ.get("WHATSAPP_CLOUD_NUMBER_ID")
+app = Flask(__name__)
 
-    def __init__(self):
-        self.headers = {
-            "Authorization": f"Bearer {self.WHATSAPP_API_TOKEN}",
-            "Content-Type": "application/json",
-        }
-        self.API_URL = self.API_URL + self.WHATSAPP_CLOUD_NUMBER_ID
+VERIFY_TOKEN = os.environ.get('WHATSAPP_HOOK_TOKEN')
 
-    def send_template_message(self, template_name, language_code, phone_number):
 
-        payload = {
-            "messaging_product": "whatsapp",
-            "to": phone_number,
-            "type": "template",
-            "template": {
-                "name": template_name,
-                "language": {
-                    "code": language_code
-                }
-            }
-        }
-        print(self.WHATSAPP_API_TOKEN)
-        response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
-        print(response)
-        print(response.status_code)
-        print(response.text)
+@app.route("/")
+def hello_world():
+    return "Hello World!"
 
-        assert response.status_code == 200, "Error sending message"
-        return response.status_code
-    
-    def send_text_message(self,message, phone_number):
-        payload = {
-            "messaging_product": 'whatsapp',
-            "to": phone_number,
-            "type": "text",
-            "text": {
-                "preview_url": False,
-                "body": message
-            }
-        }
-        response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
-        print(response.status_code)
-        print(response.text)
-        assert response.status_code == 200, "Error sending message"
-        return response.status_code
-    
-    def process_notification(self, data):
-        entries = data["entry"]
-        for entry in entries:
-            for change in entry["changes"]:
-                value = change["value"]
-                if value:
-                    if "messages" in value:
-                        for message in value["messages"]:
-                            if message["type"] == "text":
-                                from_no = message["from"]
-                                message_body = message["text"]["body"]
-                                prompt = message_body
-                                print(f"Ack from FastAPI-WtsApp Webhook: {message_body}")
-                                return {
-                                    "statusCode": 200,
-                                    "body": prompt,
-                                    "from_no": from_no,
-                                    "isBase64Encoded": False
-                                }
 
-        return {
-            "statusCode": 403,
-            "body": json.dumps("Unsupported method"),
-            "isBase64Encoded": False
-        }
+@app.route("/send_message/", methods=["POST"])
+def send_message():
+    """_summary_: Send a message with a template to a phone number"""
 
-app = FastAPI()
+    if "language_code" not in request.json:
+        return jsonify({"error": "Missing language_code"}), 400
 
-WHATSAPP_HOOK_TOKEN = os.environ.get("WHATSAPP_HOOK_TOKEN")
+    if "phone_number" not in request.json:
+        return jsonify({"error": "Missing phone_number"}), 400
 
-@app.get("/")
-def I_am_alive():
-    return "¡Estoy vivo!"
-    
-@app.get("/webhook/")
-def subscribe(request: Request):
-    print("Se ha llamado a subscribe")
+    if "template_name" not in request.json:
+        return jsonify({"error": "Missing template_name"}), 400
+
+    client = WhatsAppWrapper()
+
+    response = client.send_template_message(
+        template_name=request.json["template_name"],
+        language_code=request.json["language_code"],
+        phone_number=request.json["phone_number"],
+    )
+
+    return jsonify(
+        {
+            "data": response,
+            "status": "success",
+        },
+    ), 200
+
+
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    """__summary__: Get message from the webhook"""
+
     if request.method == "GET":
-        if request.query_params.get('hub.verify_token') == WHATSAPP_HOOK_TOKEN:
-            return int(request.query_params.get('hub.challenge'))
+        if request.args.get('hub.verify_token') == VERIFY_TOKEN:
+            return request.args.get('hub.challenge')
         return "Authentication failed. Invalid Token."
+
+    client = WhatsAppWrapper()
+
+    client.process_webhook_notification(request.get_json())
     
+    # Do anything with the response
+    # Sending a message to a phone number to confirm the webhook is working
+    # client.send_template_message("hello_world", "en_US", os.environ.get("WHATSAPP_NUMBER_WEBHOOK_TEST"))
 
-@app.post("/webhook/")
-async def process_notifications(request: Request):
-    print("Se ha llamado a callback")
-    wtsapp_client = WhatsAppWrapper()
-    data = await request.json()
-    print ("We received " + str(data))
-    response = wtsapp_client.process_notification(data)
-    if response["statusCode"] == 200:
-        if response["body"] and response["from_no"]:
-            openai_client = OpenAIClient()
-            reply = openai_client.complete(prompt=response["body"])
-            print ("\nreply is:"  + reply)
-            wtsapp_client.send_text_message(message=reply, phone_number=response["from_no"], )
-            print ("\nreply is sent to whatsapp cloud:" + str(response))
-
-    return jsonable_encoder({"status": "success"}, 200)
+    return jsonify({"status": "success"}, 200)
